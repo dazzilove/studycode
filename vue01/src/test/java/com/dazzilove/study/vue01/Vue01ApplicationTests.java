9ee9eb37b94dc7d49b2891d6f72c28b97acd9837
dazzilove <dazzilove@gmail.com>
2020-01-15 00:03:28 +0900

    프로젝트 생성

---
+++ vue01/src/test/java/com/dazzilove/study/vue01/Vue01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazzilove.study.vue01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vue01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
