52fed3a72de1cf116bb3900c45e71766e7970e91
dazzilove <dazzilove@gmail.com>
2020-02-02 14:02:26 +0900

    new

---
+++ css-2019-spring-cloud/circuit-breaker-01/customer-service/src/main/java/com/dazzilove/circuitbreaker/customer/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.dazzilove.circuitbreaker.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		run(CustomerApplication.class, args);
+	}
+
+}
